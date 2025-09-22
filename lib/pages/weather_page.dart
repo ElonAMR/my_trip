5ee13c644af5f9586bf4eae83e66d88b079dcf84
Elon Amram <elonamrammm1234@gmail.com>
2025-09-22 17:37:40 +0300

    Persist activities & budget locally: - Lifted state to Home - Added SharedPreferences (JSON) save/load - Implemented budget categories (planned vs actual) + expenses - Fixed date/time pickers context

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
-const _apiKey = 'PASTE_YOUR_OPENWEATHER_API_KEY_HERE';
+const _apiKey = '';
 
 class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
